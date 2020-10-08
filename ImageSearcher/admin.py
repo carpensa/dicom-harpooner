9c0aa123137851ea23b4533594d6d81dba6d3a91
carpensa <carpensa@ohsu.edu>
2020-10-08 13:41:06 -0700

    Operational Error addressed by adding Search to models in admin.py

--- ImageSearcher/admin.py
+++ ImageSearcher/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from dicoms.models import Subject
 from dicoms.models import Session
 from dicoms.models import Series
+from dicoms.models import Search
 
 admin.site.register(Session)
 admin.site.register(Subject)
 admin.site.register(Series)
+admin.site.register(Search)
